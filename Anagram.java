f2bbf558da60e046e0d4e4b8b30a6033095886c3
kunalvvs <kunalvvs@gmail.com>
2024-03-04 10:26:59 +0530

    fbdfb

--- Anagram.java
+++ Anagram.java
@@ -1,5 +1,7 @@
 class Anagram{
-    main
+ public static void main(String[] args) {
+    System.out.println("dvdv");
+ }
+ 
 }
-  git config --global user.email "kunalvvs@gmail.com"
-  git config --global user.name "kunalvvs"
+  
